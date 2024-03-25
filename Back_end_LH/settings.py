bdd1b3285c7a3f560729a6c754b5c7bebd5628f5
AbdurrohmanJCom <tuckerticket@gmail.com>
2024-03-25 22:22:37 +0500

    .

--- Back_end_LH/settings.py
+++ Back_end_LH/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:5173",
-    # "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'Back_end_LH.urls'
